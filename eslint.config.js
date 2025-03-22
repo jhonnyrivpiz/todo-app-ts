76add56210f81faacf5b748159c381432f1a5fdb
Jhonny Rivera Pizarro <jhonnyrivpiz@gmail.com>
2025-03-21 23:51:44 -0500

    👍Inicio Proyecto Todo: React + Ts + Vite + SWC

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import react from "@vitejs/plugin-react-swc";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { "react/react-in-jsx-scope": "off" },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended
+]);
